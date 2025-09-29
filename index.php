1d9d68303c9d1af7fa56f5779466950b7fe02062
theo.bggtt <theo.bggtt@eduge.ch>
2025-09-29 08:31:52 +0200

    changed text

--- index.php
+++ index.php
@@ -25,12 +25,12 @@
 <body>
     <header>
         <nav>
-            <a href="index.php">Accueil</a>
+            <a href="index.php"><?php echo $lang['MAINPAGE_TITLE'];?></a>
             <a href="developers.php">Dev</a>
             <a href="projects.php">Projets</a>
             <a href="contact.php">Contact</a>
         </nav>
-        <h1>Accueil</h1>
+        <h1><?php echo $lang['MAINPAGE_TITLE'];?></h1>
         <div>
             <a href="./?langue=fr">Francais</a>
             <a href="./?langue=en">English</a>
@@ -41,6 +41,9 @@
     <main>
         
         <div class="slideshow-container">
+            <div class="slideshow-title">
+                <h2><?php echo $lang['SLIDESHOW_TITLE'];?></h2>
+            </div>
             <div class="slideshow-content">
                 <img src="https://limeswood.ae/wp-content/uploads/2023/01/6.jpg" alt="">
             </div>
@@ -55,12 +58,7 @@
         <div class="presentation">
             <div>
                 <h2><?php echo $lang['THE_CONCEPT_TITLE'];?></h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla bibendum nec sapien nec aliquam. Nulla urna libero, sollicitudin eu blandit sit amet, convallis ac magna. Aenean malesuada nec nibh in tincidunt. Integer euismod tristique eros eget molestie. Vivamus malesuada nunc augue. Nullam turpis ipsum, tincidunt blandit porta at, luctus id lectus. Vivamus congue mauris sapien, ullamcorper fringilla turpis convallis ut.
-                    <br><br>                    
-
-                    Aenean eros ex, eleifend quis porta sit amet, interdum vel eros. Vestibulum congue porta justo, ut varius diam efficitur eu. Maecenas arcu nisi, tincidunt luctus sem ullamcorper, malesuada consequat massa. Curabitur ut eros tincidunt, mollis lacus at, congue erat. In eu nulla vel magna dignissim euismod ac a arcu. Donec ullamcorper neque a sem sollicitudin, laoreet suscipit mauris pulvinar. Cras lobortis dictum hendrerit. Curabitur ultricies lectus nibh, sed vehicula nibh vulputate vitae. Nulla facilisi. Praesent odio orci, ornare ac sapien nec, feugiat bibendum enim.
-                </p>
+                <p> <?php echo $lang['THE_CONCEPT_TEXT'];?></p>
             </div>
             <img src="https://limeswood.ae/wp-content/uploads/2023/01/6.jpg" alt="">
         </div>
@@ -68,9 +66,9 @@
 
     <footer>
         <div class="socials">
-            <a href="instagram.com">Instagram</a>
-            <a href="facebook.com">Facebook</a>
-            <a href="tiktok.com">Tik Tok</a>   
+            <a href="https://instagram.com">Instagram</a>
+            <a href="https://facebook.com">Facebook</a>
+            <a href="https://tiktok.com">TikTok</a>   
         </div>
     </footer>
     
--- locale/en.php
+++ locale/en.php
@@ -1,4 +1,8 @@
 <?php   
     $lang['MAINPAGE_TITLE'] = 'Homepage';
     $lang['THE_CONCEPT_TITLE'] = 'The Concept';
+    $lang['SLIDESHOW_TITLE'] = 'The elegant way of comparing';
+    $lang['THE_CONCEPT_TEXT'] = 'Discover Dubai’s real estate like never before. Our platform brings together every upcoming and ongoing project in one place, making it easy to explore, compare, and review developments across the city. From prices and handover dates to developer track records and community insights, you’ll find everything you need to make confident property decisions. <br><br>Think of it as the <b>“Tripadvisor for Dubai real estate”</b> a trusted guide where buyers, investors, and residents can share experiences, rate projects, and uncover the best opportunities in one of the world’s most exciting property markets.';
+
+                
 ?>
